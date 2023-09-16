75569fc920296dbb76249960a1a17a483220b2cb
xImoZA <alekcandra.ri@gmail.com>
2023-09-16 17:43:47 +0300

    add task-1

---
+++ src/homeworks/homework-1/Task-1_Ri.py
@@ -0,0 +1,8 @@
+def polynomial(x):
+    square_of_x = x * x
+    return (square_of_x + 1) * (square_of_x + x) + 1
+
+
+if __name__ == '__main__':
+    input_x = int(input('Calculating x^4+x^3+x^2+x+1, enter x:'))
+    print(f'{input_x}^4+{input_x}^3+{input_x}^2+{input_x}+1={polynomial(input_x)}')
