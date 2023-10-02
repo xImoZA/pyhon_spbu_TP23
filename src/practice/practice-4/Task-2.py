d6738d8875f66a15980aff42bb95b340cbcd2e66
xImoZA <alekcandra.ri@gmail.com>
2023-10-02 17:04:32 +0300

    use black

--- src/practice/practice-4/Task-2.py
+++ src/practice/practice-4/Task-2.py
@@ -26,22 +26,24 @@ def count_bulls_and_cows(input_, answer):
     return number_cows - number_bulls, number_bulls
 
 
-if __name__ == '__main__':
-    print('Bulls and cows\n It is necessary to guess a four-digit digit number consisting of non-repeating digits\n'
-          'After each move, the computer outputs the number of cows(the number of guessed digits in the wrong '
-          'positions) and the number of bulls(the number of guessed digits in their correct positions)')
+if __name__ == "__main__":
+    print(
+        "Bulls and cows\n It is necessary to guess a four-digit digit number consisting of non-repeating digits\n"
+        "After each move, the computer outputs the number of cows(the number of guessed digits in the wrong "
+        "positions) and the number of bulls(the number of guessed digits in their correct positions)"
+    )
 
     answer_number = create_number()
 
     attempts = 0
     while True:
         attempts += 1
-        input_number = input(f'{attempts} attempt. To enter a number: ')
+        input_number = input(f"{attempts} attempt. To enter a number: ")
 
         if input_number != answer_number:
             cows, bulls = count_bulls_and_cows(input_number, answer_number)
-            print(f'Found {cows} cows and {bulls} bulls. Try again')
-        
+            print(f"Found {cows} cows and {bulls} bulls. Try again")
+
         else:
-            print(f'You won in {attempts} moves')
+            print(f"You won in {attempts} moves")
             break
