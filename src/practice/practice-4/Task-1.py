ed01fc6388add64a3b228d5f6dc98ab571967bca
xImoZA <alekcandra.ri@gmail.com>
2023-10-02 17:02:00 +0300

    use black

--- src/practice/practice-4/Task-1.py
+++ src/practice/practice-4/Task-1.py
@@ -6,10 +6,15 @@ def get_fractions(max_denominator):
     for denominator in range(1, max_denominator + 1):
         for numerator in range(1, denominator):
             if gcd(numerator, denominator) == 1:
-                dict_output_fractions[numerator/denominator] = f'{numerator}/{denominator}'
-    return [dict_output_fractions[fractions] for fractions in sorted(dict_output_fractions.keys())]
+                dict_output_fractions[
+                    numerator / denominator
+                ] = f"{numerator}/{denominator}"
+    return [
+        dict_output_fractions[fractions]
+        for fractions in sorted(dict_output_fractions.keys())
+    ]
 
 
-if __name__ == '__main__':
-    n = int(input('To output all irreducible fractions, enter n: '))
+if __name__ == "__main__":
+    n = int(input("To output all irreducible fractions, enter n: "))
     print(f'Fractions: {" ".join(get_fractions(n))}')
