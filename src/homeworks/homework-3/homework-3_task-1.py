9ec7a10f16abfe016613e8cbe235028511702e18
xImoZA <alekcandra.ri@gmail.com>
2023-10-11 01:42:35 +0300

    add full task 1

--- src/homeworks/homework-3/homework-3_task-1.py
+++ src/homeworks/homework-3/homework-3_task-1.py
@@ -1,17 +1,21 @@
 def curry_explicit(function, arity):
     if arity < 0:
-        print("Uncorrected arity")
-        return False
+        exit("Uncorrected arity")
 
     elif arity == 0:
-        return function
+        return function()
 
     def get_args(args):
+        if len(args) > arity:
+            exit(
+                "Uncorrected input: The number of arguments does not correspond to arity"
+            )
+
         if len(args) == arity:
             return function(*args)
 
-        def get_one_arg(arg):
-            return get_args([*args, arg])
+        def get_one_arg(*new_arg):
+            return get_args([*args, *new_arg])
 
         return get_one_arg
 
@@ -20,25 +24,33 @@ def curry_explicit(function, arity):
 
 def uncurry_explicit(function, arity):
     if arity < 0:
-        print("Uncorrected arity")
-        return False
+        exit("Uncorrected arity")
 
     elif arity == 0:
-        return function
+        return function()
 
     def get_args(*args):
         if len(args) != arity:
-            print("Error: The number of arguments does not correspond to arity")
-            return False
+            exit(
+                "Uncorrected input: The number of arguments does not correspond to arity"
+            )
 
-    # у меня совершенно нет идей, помогите пожалуйста
-    # я не понимаю как аргументы поместить в скобки и эти скобки подряд в функцию(
+        output_function = function(args[0])
+
+        for i in range(1, len(args)):
+            output_function = output_function(args[i])
+
+        return output_function
 
     return get_args
 
 
-if __name__ == "__main__":
+def main():
     f2 = curry_explicit((lambda x, y, z: f"<{x},{y},{z}>"), 3)
-    # g2 = uncurry_explicit(f2, 3)
-    print(f2(123)(456)(562)(123))  # <123,456,562>
-    # print(g2(123, 456, 562))  # <123,456,562>
+    g2 = uncurry_explicit(f2, 3)
+    print(f2(123)(456)(562))  # <123,456,562>
+    print(g2(123, 456, 562))  # <123,456,562>
+
+
+if __name__ == "__main__":
+    main()
