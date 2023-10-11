02a1cfddd5830bc286c6279626e6fda0ee036e57
xImoZA <alekcandra.ri@gmail.com>
2023-10-11 15:07:25 +0300

    fix 1

--- src/tests/test-1/test-1_task-1.py
+++ src/tests/test-1/test-1_task-1.py
@@ -6,12 +6,15 @@ def sort_file(name_read_file, name_write_file, input_a, input_b):
     less_a = []
     more_a_less_b = []
     more_b = []
+
     with open(name_read_file, "r") as file:
         for number in list(map(int, file.readline().split())):
             if number < input_a:
                 less_a += [str(number)]
+
             elif input_a <= number <= input_b:
                 more_a_less_b += [str(number)]
+
             else:
                 more_b += [str(number)]
 
@@ -30,8 +33,10 @@ if __name__ == "__main__":
 
     if not os.path.exists(f):
         print(f"File {f} not exist")
+
     elif os.path.exists(g):
         print(f"File {g} already exist")
+
     else:
         sort_file(f, g, a, b)
         print(f"The sorted numbers is written to {g}")
