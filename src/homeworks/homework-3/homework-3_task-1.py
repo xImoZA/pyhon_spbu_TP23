b55cb5ec2f372108594067419a7f3e70d1ba68f5
xImoZA <alekcandra.ri@gmail.com>
2023-10-17 15:50:57 +0300

    fix 1

--- src/homeworks/homework-3/homework-3_task-1.py
+++ src/homeworks/homework-3/homework-3_task-1.py
@@ -1,18 +1,18 @@
 def curry_explicit(function, arity):
     if arity < 0:
-        exit("Uncorrected arity")
-
-    elif arity == 0:
-        return function
+        raise ValueError("Arity must be non-negative")
 
     def get_args(args):
-        if len(args) > arity:
-            exit(
-                "Uncorrected input: The number of arguments does not correspond to arity"
-            )
-
         if len(args) == arity:
-            return function(*args)
+            try:
+                if arity == 0:
+                    return function
+
+                return function(*args)
+            except TypeError:
+                raise ValueError(
+                    "Uncorrected input: The number of arguments does not correspond to arity"
+                )
 
         def get_one_arg(*new_arg):
             return get_args([*args, *new_arg])
@@ -24,16 +24,16 @@ def curry_explicit(function, arity):
 
 def uncurry_explicit(function, arity):
     if arity < 0:
-        exit("Uncorrected arity")
-
-    elif arity == 0:
-        return function
+        raise ValueError("Arity must be non-negative")
 
     def get_args(*args):
         if len(args) != arity:
-            exit(
+            raise ValueError(
                 "Uncorrected input: The number of arguments does not correspond to arity"
             )
+
+        if arity == 0:
+            return function()
 
         output_function = function(args[0])
 
