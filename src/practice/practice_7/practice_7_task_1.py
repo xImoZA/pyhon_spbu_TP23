c66d5a60cc4587dd4a093cce60de10488ac4ea97
xImoZA <alekcandra.ri@gmail.com>
2023-10-25 15:52:59 +0300

    Fix main.yaml, add indication an error in get_corrected_input, fix is_float_numbers for one element, rename solution_equation to solve_equation, fix use tuples, add error for x can be anything, fix solution_quadratic_equation exception sort, fix separation tests, fix use tuples

--- src/practice/practice_7/practice_7_task_1.py
+++ src/practice/practice_7/practice_7_task_1.py
@@ -1,33 +1,47 @@
 def main():
     input_coefficients = input("Enter the coefficients of the equation: ").split()
-    while not get_corrected_input(input_coefficients):
-        input_coefficients = input("Incorrect input. Try again: ")
+    is_input_correct(input_coefficients)
     coefficients = list(map(float, input_coefficients))
 
-    print(
-        f"Solution of the equation: {' '.join(map(str, solution_equation(coefficients[0], coefficients[1], coefficients[2])))}"
+    solve_of_equation = get_beautiful_numbers(
+        solve_equation(coefficients[0], coefficients[1], coefficients[2])
     )
+    print(f"Solution of the equation: {' '.join(map(str, solve_of_equation))}")
 
 
-def get_corrected_input(user_input):
-    if len(user_input) != 3 or not is_float_numbers(user_input):
+def get_beautiful_numbers(answer):
+    beautiful_numbers = []
+    for number in answer:
+        if int(number) == number:
+            beautiful_numbers.append(int(number))
+
+        else:
+            beautiful_numbers.append(number)
+
+    return tuple(beautiful_numbers)
+
+
+def is_input_correct(user_input):
+    if len(user_input) != 3:
+        raise ValueError("More than 3 arguments have been entered")
+
+    for i in range(len(user_input)):
+        if not is_float_number(user_input[i]):
+            raise ValueError(f"Invalid argument №{i + 1}")
+
+
+def is_float_number(number):
+    try:
+        float(number)
+        return True
+
+    except ValueError:
         return False
-    return True
 
 
-def is_float_numbers(numbers):
-    for number in numbers:
-        try:
-            float(number)
-        except ValueError:
-            return False
-
-    return True
-
-
-def solution_equation(a, b, c):
+def solve_equation(a, b, c):
     if a == b == c == 0:
-        return ["X can be anything"]
+        raise ValueError("X can be anything")
 
     elif a == b == 0:
         raise ValueError("The equation has no solutions")
@@ -46,16 +60,13 @@ def solution_quadratic_equation(a, b, c):
         raise ValueError("The discriminant is less than zero")
 
     elif discriminant == 0:
-        return [-b / (2 * a)]
+        return (-b / (2 * a),)
 
-    res = [(-b + discriminant**0.5) / (2 * a), (-b - discriminant**0.5) / (2 * a)]
-    res.sort()
-
-    return res
+    return (-b + discriminant**0.5) / (2 * a), (-b - discriminant**0.5) / (2 * a)
 
 
 def solving_linear_equation(b, c):
-    return [-c / b]
+    return (-c / b,)
 
 
 if __name__ == "__main__":
--- tests/practice/practice-7/test_task-1.py
+++ tests/practice/practice-7/test_task-1.py
@@ -5,64 +5,89 @@ import pytest
 
 @pytest.mark.parametrize(
     "numbers,expected",
-    [([1, 2, 3], True), (["w", 2, 3], False), (["#", "w", "4"], False)],
+    [
+        ("1", True),
+        ("-1", True),
+        ("-1.5", True),
+        ("w", False),
+        ("-w", False),
+        ("-w.e", False),
+        ("w.2", False),
+        ("-w.2", False),
+    ],
 )
 def test_is_float_numbers(numbers, expected):
-    actual = is_float_numbers(numbers)
+    actual = is_float_number(numbers)
     assert actual == expected
 
 
 @pytest.mark.parametrize(
     "user_input,expected",
     [
-        (["1", "2", "3"], True),
-        (["1", "2", "3", "4"], False),
-        (["w", "m", "q"], False),
-        (["w", "m", "q", "#"], False),
-        (["1", "2", "w"], False),
-        (["w", "2", "m", "3"], False),
+        (["1", "2", "3"], None),
     ],
 )
-def test_get_corrected_input(user_input, expected):
-    actual = get_corrected_input(user_input)
+def test_is_input_correct(user_input, expected):
+    actual = is_input_correct(user_input)
     assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "user_input",
+    [
+        (["1", "2", "3", "4"]),
+        (["w", "m", "q"]),
+        (["w", "m", "q", "#"]),
+        (["1", "2", "w"]),
+        (["w", "2", "m", "3"]),
+    ],
+)
+def test_is_input_correct(user_input):
+    with pytest.raises(ValueError):
+        is_input_correct(user_input)
 
 
 @pytest.mark.parametrize(
     "a,b,c,expected",
     [
-        (3, 0, 0, [0]),
-        (-3, 0, 0, [0]),
+        (3, 0, 0, (0,)),
+        (-3, 0, 0, (0,)),
         (2, 0, -18, (-3, 3)),
         (-2, 0, 18, (-3, 3)),
-        (2, 0, 18, ValueError),
-        (-2, 0, -18, ValueError),
         (3, 6, 0, (0, -2)),
         (3, -6, 0, (0, 2)),
         (-3, 6, 0, (0, 2)),
         (-3, -6, 0, (0, -2)),
-        (1, 2, 3, ValueError),
         (2, -1, -15, (-2.5, 3)),
-        (1, 4, 4, [-2]),
+        (1, 4, 4, (-2,)),
     ],
 )
 def test_solution_quadratic_equation(a, b, c, expected):
-    if expected == ValueError:
-        with pytest.raises(ValueError):
-            solution_quadratic_equation(a, b, c)
+    actual = solution_quadratic_equation(a, b, c)
+    if len(actual) == 2:
+        assert len(actual) == len(expected) and (
+            (actual[0] == expected[0] and actual[1] == expected[1])
+            or (actual[0] == expected[1] and actual[1] == expected[0])
+        )
     else:
-        actual = solution_quadratic_equation(a, b, c)
-        if len(actual) == 2:
-            assert len(actual) == len(expected) and (
-                (actual[0] == expected[0] and actual[1] == expected[1])
-                or (actual[0] == expected[1] and actual[1] == expected[0])
-            )
-        else:
-            assert len(actual) == len(expected) and actual == expected
+        assert len(actual) == len(expected) and actual == expected
 
 
 @pytest.mark.parametrize(
-    "b,c,expected", [(2, 1, [-0.5]), (-2, 1, [0.5]), (2, -6, [3.0])]
+    "a,b,c",
+    [
+        (2, 0, 18),
+        (-2, 0, -18),
+        (1, 2, 3),
+    ],
+)
+def test_errors_solution_quadratic_equation(a, b, c):
+    with pytest.raises(ValueError):
+        solution_quadratic_equation(a, b, c)
+
+
+@pytest.mark.parametrize(
+    "b,c,expected", [(2, 1, (-0.5,)), (-2, 1, (0.5,)), (2, -6, (3.0,))]
 )
 def test_solving_linear_equation(b, c, expected):
     actual = solving_linear_equation(b, c)
@@ -72,27 +97,42 @@ def test_solving_linear_equation(b, c, expected):
 @pytest.mark.parametrize(
     "a,b,c,expected",
     [
-        (0, 0, 0, ["X can be anything"]),
-        (0, 0, 5, ValueError),
-        (0, 2, 1, [-0.5]),
-        (2, 0, 18, ValueError),
+        (0, 2, 1, (-0.5,)),
         (2, -1, -15, (-2.5, 3)),
-        (1, 4, 4, [-2]),
+        (1, 4, 4, (-2,)),
     ],
 )
-def test_solving_equation(a, b, c, expected):
-    if expected == ValueError:
-        with pytest.raises(ValueError):
-            solution_equation(a, b, c)
+def test_solve_equation(a, b, c, expected):
+    actual = solve_equation(a, b, c)
+    if len(actual) == 2:
+        assert len(actual) == len(expected) and (
+            (actual[0] == expected[0] and actual[1] == expected[1])
+            or (actual[0] == expected[1] and actual[1] == expected[0])
+        )
     else:
-        actual = solution_equation(a, b, c)
-        if len(actual) == 2:
-            assert len(actual) == len(expected) and (
-                (actual[0] == expected[0] and actual[1] == expected[1])
-                or (actual[0] == expected[1] and actual[1] == expected[0])
-            )
-        else:
-            assert len(actual) == len(expected) and actual == expected
+        assert len(actual) == len(expected) and actual == expected
+
+
+@pytest.mark.parametrize(
+    "a,b,c",
+    [
+        (0, 0, 0),
+        (0, 0, 5),
+        (2, 0, 18),
+    ],
+)
+def test_errors_solve_equation(a, b, c):
+    with pytest.raises(ValueError):
+        solve_equation(a, b, c)
+
+
+@pytest.mark.parametrize(
+    "numbers,expected",
+    [(-0.0, (0,)), (-2.5, (-2.5,)), (2.0, (2,))],
+)
+def test_get_beautiful_numbers(numbers, expected):
+    actual = get_beautiful_numbers(numbers)
+    assert actual == expected
 
 
 def test_main(monkeypatch):
@@ -102,6 +142,6 @@ def test_main(monkeypatch):
     main()
     output = fake_output.getvalue()
     assert (
-        output == "Solution of the equation: -2.5 3.0\n"
-        or output == "Solution of the equation: 3.0 -2.5\n"
+        output == "Solution of the equation: -2.5 3\n"
+        or output == "Solution of the equation: 3 -2.5\n"
     )
